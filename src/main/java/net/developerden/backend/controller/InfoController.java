701856643075c8ac3b390c8a343c44f5ed0beb63
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-06-21 16:58:29 +0100

    bit of cleanup and documentation

--- src/main/java/net/developerden/backend/controller/InfoController.java
+++ src/main/java/net/developerden/backend/controller/InfoController.java
@@ -2,7 +2,6 @@ package net.developerden.backend.controller;
 
 import net.developerden.backend.security.DiscordUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +10,6 @@ public class InfoController {
 
     @GetMapping("/user")
     public String user(@AuthenticationPrincipal DiscordUser principal) {
-        return "Hello, " + principal.getName();
+        return "Hello, %s (%s)".formatted(principal.fullName(), principal.id());
     }
 }
--- src/main/java/net/developerden/backend/security/DiscordUser.java
+++ src/main/java/net/developerden/backend/security/DiscordUser.java
@@ -6,8 +6,13 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.Collection;
 import java.util.Map;
 
+/* TODO we might wanna convert this to a database entity in the future so we can add more data*/
+
+/**
+ * Java representation of all of the data that Discord gives us from the /users/@me API endpoint.
+ */
 public record DiscordUser(String id, String username, String discriminator, String locale) implements OAuth2User {
-    private String fullName() {
+    public String fullName() {
         return username + "#" + discriminator;
     }
 
